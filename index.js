6ae66329a6116808ae712c1e361cb68a132cc115
abdulhamid331404 <freelancerhamid1@gmail.com>
2022-10-26 16:44:46 +0600

    added courses

--- index.js
+++ index.js
@@ -5,10 +5,25 @@ app.use(cors());
 
 const port = process.env.PORT || 5000;
 
+const courses = require('./data/courses.json')
+
 app.get('/', (req, res)=>{
     res.send('All Course Cumming soon')
 });
 
+app.get('/courses', (req, res)=>{
+    res.send(courses)
+});
+
+app.get('/courses/:id', (req, res)=>{
+   const id = req.params.id;
+   const getSingleItem = courses?.find(c => c.id == id);
+   res.send(getSingleItem)
+});
+
+
+
+
 app.listen(port, ()=>{
-    console.log('port is running', port);
+    console.log('learning server is running', port);
 })
